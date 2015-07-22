a24f62c1ec2beed05fb06854697b37b6725bf895
Elad Bezalel <elad.bezalel@gmail.com>
2015-07-22 13:03:52 +0300

    Finished testing - 100% coverage

--- test/main.spec.js
+++ test/main.spec.js
@@ -250,6 +250,24 @@ describe('packages', function () {
       });
     });
 
+    describe('with existing package in dips folder and no permissions', function () {
+      before(function () {
+        var jfMock = {
+          readFile: sinon.stub().callsArgWith(1, {code: 'ENOPER'})
+        };
+
+        mockery.registerMock('jsonfile', jfMock);
+      });
+
+      after(function () {
+        mockery.deregisterMock('jsonfile');
+      });
+
+      it('should return \'test\' package', function () {
+        return expect(packages.getPackage('test', 'dip')).to.eventually.be.rejectedWith({code: 'ENOPER'});
+      });
+    });
+
     describe('with non-existing package in dips folder', function () {
       it('should be rejected with error "dip doesn\'t exist"', function () {
         return expect(packages.getPackage('test', 'dip')).to.eventually.be.rejectedWith('dip doesn\'t exist');
@@ -286,6 +304,24 @@ describe('packages', function () {
       describe('with no packages in folder', function () {
         it('should eventually be empty array', function () {
           return expect(packages.getByType('dip')).to.eventually.be.empty.array;
+        });
+      });
+
+      describe('with no access to folder', function () {
+        before(function () {
+          var fsMock = {
+            readdir: sinon.stub().callsArgWith(1, {code: 'ENOPER'})
+          };
+
+          mockery.registerMock('fs', fsMock);
+        });
+
+        after(function () {
+          mockery.deregisterMock('fs');
+        });
+
+        it('should eventually be rejected', function () {
+          return expect(packages.getByType('dip')).to.eventually.be.rejectedWith({code: 'ENOPER'});
         });
       });
       describe('with some packages', function () {
@@ -333,6 +369,28 @@ describe('packages', function () {
                 });
               });
           });
+        });
+      });
+      describe('with some files', function () {
+        before(function () {
+          var fsMock = {
+            readdir: sinon.stub().callsArgWith(1, null, ['file1', 'file2']),
+            stat: sinon.stub().callsArgWith(1, null, {
+              isDirectory: function () {
+                return false;
+              }
+            })
+          };
+
+          mockery.registerMock('fs', fsMock);
+        });
+
+        after(function () {
+          mockery.deregisterMock('fs');
+        });
+
+        it('should return list of packages', function () {
+          return expect(packages.getByType('dip')).to.eventually.have.length(0);
         });
       });
     });
@@ -464,6 +522,94 @@ describe('packages', function () {
 
       it('should eventually return the config of the dip', function () {
         return expect(packages.getTaco('test')).to.eventually.to.have.property('config', null);
+      });
+    });
+  });
+
+  describe('get all', function () {
+    var packages;
+
+    beforeEach(function () {
+      var nachosConfigMock = function () {
+        return {
+          get: sinon.stub().returns(Q.resolve({packages: 'path'}))
+        };
+      };
+
+      mockery.registerMock('nachos-config', nachosConfigMock);
+      mockery.enable({
+        useCleanCache: true,
+        warnOnReplace: false,
+        warnOnUnregistered: false
+      });
+
+      packages = require('../lib');
+    });
+
+    afterEach(function () {
+      mockery.deregisterMock('nachos-config');
+      mockery.disable();
+    });
+
+    describe('with \'full\' option', function () {
+      before(function () {
+        var fsMock = {
+          readdir: sinon.stub().callsArgWith(1, null, ['pkg1', 'pk22']),
+          stat: sinon.stub().callsArgWith(1, null, {
+            isDirectory: function () {
+              return true;
+            }
+          })
+        };
+        var jfMock = {
+          readFile: sinon.stub().callsArgWith(1, null, {test: 'test'})
+        };
+
+        mockery.registerMock('jsonfile', jfMock);
+        mockery.registerMock('fs', fsMock);
+      });
+
+      after(function () {
+        mockery.deregisterMock('fs');
+        mockery.deregisterMock('jsonfile');
+      });
+
+      it('should return dictionary with 2 types filled with packages with config property', function () {
+        return packages.getAll(true)
+          .then(function (allPackages) {
+            Object.keys(allPackages).forEach(function (key) {
+              allPackages[key].forEach(function (pkg) {
+                expect(pkg).to.have.deep.property('config');
+              });
+            });
+          });
+      });
+    });
+
+    describe('without \'full\' option', function () {
+      before(function () {
+        var fsMock = {
+          readdir: sinon.stub().callsArgWith(1, null, ['pkg1', 'pk22']),
+          stat: sinon.stub().callsArgWith(1, null, {
+            isDirectory: function () {
+              return true;
+            }
+          })
+        };
+
+        mockery.registerMock('fs', fsMock);
+      });
+
+      after(function () {
+        mockery.deregisterMock('fs');
+      });
+
+      it('should return dictionary with 2 types filled with packages', function () {
+        return packages.getAll()
+          .then(function (allPackages) {
+            expect(allPackages).to.have.property('dip');
+            expect(allPackages).to.have.property('taco');
+          });
       });
     });
   });
